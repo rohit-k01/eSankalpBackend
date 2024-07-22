888fde4256ed9446a1c8b147b63a822a30513a0d
rohit <rohit@ASUSVIVOBOOK_>
2024-07-22 22:16:11 +0530

    Add project files.

---
+++ eSankalpBackend/Controllers/CategorieController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eSankalpBackend.Controllers
+{
+    public class CategorieController : Controller
+    {
+        // GET: Categorie
+        public ActionResult CategoryIndex()
+        {
+            return View();
+        }
+    }
+}
